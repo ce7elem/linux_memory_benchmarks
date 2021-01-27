4faf68d9a77b735607fb482c05fa6d1707d5ae1a
TERRUSS <olivier.benaben@protonmail.com>
2021-01-27 19:13:53 +0100

    improving builds scripts

--- compile_all.sh
+++ compile_all.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 source colors.sh
 
-for makefile in `find -name "makefile"`; do
+PARAMS=""
+
+while (( "$#" )); do
+  case "$1" in
+    -c|--clean)
+      CLEAN=0
+      shift
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+
+# set positional arguments in their proper place
+eval set -- "$PARAMS"
+
+
+for makefile in `find . -name "makefile"`; do
   test=$(dirname $makefile)
 
-  echo -e "${LIGHTGREEN}*${NOCOLOR} compilling ${GREEN}$test${NOCOLOR} benchmark"
+  if [[ $CLEAN == 0 ]]; then
+    echo -e "${LIGHTGREEN}*${NOCOLOR} cleaning ${GREEN}$test${NOCOLOR} benchmark"
+    make clean -C ./$test | sed -e 's/^/\t/' # indent make's output
+  else
+    echo -e "${LIGHTGREEN}*${NOCOLOR} compilling ${GREEN}$test${NOCOLOR} benchmark"
+    make all -C ./$test | sed -e 's/^/\t/' # indent make's output
+  fi
 
-  make all -C ./$test | sed -e 's/^/\t/' # indent make's output
 done
