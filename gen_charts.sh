fbec871c92b3937c6cd0f491709c30e29ff0e436
TERRUSS <olivier.benaben@protonmail.com>
2021-01-28 08:01:32 +0100

    q

--- gen_charts.sh
+++ gen_charts.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 source colors.sh
 
-function getbenchParameters() {
-  # load parameters for the bench
-  bench=`echo $1 | sed -r 's/_[^_]*$//g'`
-  source ./benchs_conf/$bench.conf
-
-  #build the final string of parameters
-  res=""
-  for key in "title_" "xlabel_" "ylabel_"; do
-    res="$res ; $key='${!key}'"
-  done;
-
-  echo $res
-}
-
 # main
-for dat_file in `find -name "*.dat"`; do
-  bench=$(basename $dat_file)
-  bench="${bench%%.*}"
+for bench in `find . -name "makefile"`; do
 
-  echo -e "${LIGHTGREEN}*${NOCOLOR} found ${ORANGE}$dat_file${NOCOLOR}, generating ${GREEN}$bench${NOCOLOR} chart"
+  bench=$(dirname $bench)
+
+  echo -e "${LIGHTGREEN}*${NOCOLOR} found ${ORANGE}$bench${NOCOLOR}, generating ${GREEN}$bench${NOCOLOR} chart"
   args=$(getbenchParameters $bench)
 
-  gnuplot -e "dat_src_='$dat_file'; $args" ./benchs_conf/template.gp 2> sed -e 's/^/\t/' > charts/$bench.png
+  gnuplot -e  ./benchs_conf/${bench}.gp 2> sed -e 's/^/\t/' > charts/$bench.png
 
 done;
--- run_benchs.sh
+++ run_benchs.sh
@@ -6,7 +6,13 @@ source colors.sh
 CORE_ID=3 # the core id on which the bench is executed
 cpupower -c $CORE_ID frequency-set -f 3.50GHz --governor performance
 
+REPETITION=10000
 
+  # 2**10 = Ki ; 2**10**2 = Mi
+declare -A BENCH_ITERATIONS
+BENCH_ITERATIONS["L1"]=$((128 * 2**10))
+BENCH_ITERATIONS["L2"]=$((1 * 2**10**2))
+BENCH_ITERATIONS["L3"]=$((6 * 2**10**2))
 # subroutines
 
 function run(){
@@ -16,21 +22,14 @@ function run(){
 
   echo -e "${LIGHTGREEN}*${NOCOLOR} running bench ${GREEN}$bench${NOCOLOR}"
 
-  # fetching benchmark params
-  source ./benchs_conf/$bench.conf 2>/dev/null
-
-  [ $? == 0 ] && for iteration in ${bench_iterations[@]}; do
-    local size=${bench[$iteration,"size"]}
-    local kern_repetition=${bench[$iteration,"kern_repetition"]}
+  for iteration in ${MEM_SIZE[@]}; do
+    local size=${bench[$iteration]}
 
     # run the bench
     echo -e "\t${GREEN}>${NOCOLOR}${LIGHTGRAY} \
-taskset -c $CORE_ID $exe $size $kern_repetition | cut -d';' -f1,9 > $bench_dir/${bench}_${iteration}.dat \
+taskset -c $CORE_ID $exe $size $REPETITION > $bench_dir/${bench}_${iteration}.dat \
 ${NOCOLOR}"
-  done \
-    || echo -e "\t${RED}x${NOCOLOR} ${ORANGE} Passing, no config file @./bench_conf/$bench.conf"
-
-
+  done
 }
 
 
